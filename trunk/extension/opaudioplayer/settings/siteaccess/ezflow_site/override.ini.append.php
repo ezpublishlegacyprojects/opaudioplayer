1b2ef951f1a1545e69d13a457bc85bed246e383b
op <info@example.com>
2010-10-03 15:34:36 +0000

    Add override.ini file for full view tamplate for ezflow_site
    git-svn-id: http://svn.projects.ez.no/opaudioplayer@2 666c53f7-d1a0-4366-bf63-b93af2e47547

---
+++ trunk/extension/opaudioplayer/settings/siteaccess/ezflow_site/override.ini.append.php
@@ -0,0 +1,9 @@
+<?php /* #?ini charset="utf-8"?
+
+[full_audio_flash_player]
+Source=node/view/full.tpl
+MatchFile=full/audio_flash_player.tpl
+Subdir=templates
+Match[class_identifier]=audio_flash_player
+
+*/ ?>
