957490712bafbc396a277941ad61cc7f42cf1044
op <info@example.com>
2010-10-03 15:35:10 +0000

    Fix admin full view issue, fixed by creating a override.ini file for ezflow_site siteaccess
    git-svn-id: http://svn.projects.ez.no/opaudioplayer@3 666c53f7-d1a0-4366-bf63-b93af2e47547

--- trunk/extension/opaudioplayer/settings/override.ini.append.php
+++ trunk/extension/opaudioplayer/settings/override.ini.append.php
@@ -1,10 +1,5 @@
 <?php /* #?ini charset="utf-8"?
 
-[full_audio_flash_player]
-Source=node/view/full.tpl
-MatchFile=full/audio_flash_player.tpl
-Subdir=templates
-Match[class_identifier]=audio_flash_player
 
 [embed_audio_flash_player]
 Source=content/view/embed.tpl
